7e43fa6abc2f3b20a410af3605e74c7127fdf986
rafaelgoncalvesmatos <rafael.cgid@gmail.com>
2022-12-14 16:19:00 -0300

    update

--- first1/test1/script2.js
+++ first1/test1/script2.js
@@ -5,8 +5,11 @@ import { sleep } from 'k6';
 // k6 run script.js 
 // k6 run --vus 10 --duration 30s script.js 
 // docker run --rm -i grafana/k6 run - <script.js
-
+export const options = {
+    vus: 10,
+    duration: '30s',
+};
 export default function() {
     http.get('http://172.17.0.1');
     sleep(2);
-}
+};
